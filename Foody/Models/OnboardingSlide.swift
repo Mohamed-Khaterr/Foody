87c17ae3684ea2a815cbb03e5b2feabbe8a8a3fa
Mohamed Khater <mohamed.15751@gmail.com>
2023-05-08 18:31:35 -0700

    Create Onboarding Model

---
+++ Foody/Models/OnboardingSlide.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingSlide.swift
+//  Foody
+//
+//  Created by Khater on 5/8/23.
+//
+
+import UIKit
+
+
+struct OnboardingSlide {
+    let title: String
+    let description: String
+    let image: UIImage?
+}
