cf8c3980eb91c22e8382f1fbe41e7a5cf9edd60c
Mohamed Khater <mohamed.15751@gmail.com>
2023-05-13 07:09:30 -0700

    Create Dish Model

---
+++ Foody/Models/Dish.swift
@@ -0,0 +1,16 @@
+//
+//  Dish.swift
+//  Foody
+//
+//  Created by Khater on 5/13/23.
+//
+
+import Foundation
+
+
+struct Dish {
+    let id: String
+    let name: String
+    let description: String
+    let image: String
+}
