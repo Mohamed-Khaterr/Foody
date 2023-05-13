8b771219dd1a501e564ea06b051a4f11718475cf
Mohamed Khater <mohamed.15751@gmail.com>
2023-05-13 07:08:59 -0700

    Create FoodCategory Model

---
+++ Foody/Models/FoodCategory.swift
@@ -0,0 +1,15 @@
+//
+//  FoodCategory.swift
+//  Foody
+//
+//  Created by Khater on 5/13/23.
+//
+
+import Foundation
+
+
+struct FoodCategory {
+    let id: String
+    let name: String
+    let image: String
+}
